d4edfb7a4e3b1789c9482d99de885ca1f4695965
aa-crypto-ai <39453357+aa-crypto-ai@users.noreply.github.com>
2025-02-01 17:12:35 +0800

    Fix frontend rendering linebreaks or quotation marks, use js to render responses instead of directly from jinja2 variable

--- app/app.py
+++ app/app.py
@@ -8,6 +8,20 @@ app = Flask(__name__)
 @app.route('/chatbot', methods=["GET", "POST"])
 def chatbot():
 
+    if request.method == 'GET':
+
+        prompt = ''
+
+    if request.method == 'POST':
+
+        prompt = request.form["prompt"]
+
+    return render_template('chatbot.html', prompt=prompt)
+
+# todo: figure out how grid.js can handle async requests for each model
+# to be separated into llm's own routes, when it's of more complex project size
+@app.route('/chatbot/inference', methods=["POST"])
+def get_inference():
     # the available model names will be separated from this function
     models = [
         {'family_name': 'meta-llama', 'model_name': 'llama-3.2-1b-instruct:free'},
@@ -15,37 +29,21 @@ def chatbot():
         {'family_name': 'mistralai', 'model_name': 'mistral-small-24b-instruct-2501'},   # not free, skip during development
     ]
 
-    if request.method == 'GET':
+    prompt = request.form['prompt']
+    if not prompt:
+        return {'data': [], 'total_models': 0}
 
-        prompt = ''
+    data = []
 
-        for model in models:
-            model['output'] = 'N/A'
-
-    if request.method == 'POST':
-
-        prompt = request.form["prompt"]
-
-        for model in models:
-            text = get_response(prompt, model['family_name'], model['model_name'])
-            if text is None:
-                text = '[Internal Error] please try again'
-            # todo: need to cater for how to display new line in the output in frontend
-            model['output'] = text.replace('"', "'").replace('\n', '\\n')   # dirty way for now to avoid quotes / newline crashing the javascript in grid.js
-
-    return render_template('chatbot.html', models=models, prompt=prompt)
-
-# this function should be used, to do inference per model instead of as a whole
-# todo: figure out how grid.js can handle async requests for each model
-# to be separated into llm's own routes, when it's of more complex project size
-@app.route('/chatbot/inference', methods=["POST"])
-def get_inference():
-    prompt = 'what is CTF?'
-    family_name = 'meta-llama'
-    model_name = 'llama-3.2-1b-instruct:free'
-    text = get_response(prompt, family_name, model_name)
+    for model in models:
+        family_name = model['family_name']
+        model_name = model['model_name']
+        text = get_response(prompt, family_name, model_name)
+        data.append(
+            {'family_name': family_name, 'model_name': model_name, 'output': text}
+        )
     # todo: add evaluation results
-    return text
+    return {'data': data, 'total_models': len(data)}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
